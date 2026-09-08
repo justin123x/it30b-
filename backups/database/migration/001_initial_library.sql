0b31566debe7b683ed17531786564acb2baa0ccc
justin123x <20240217@nbsc.edu.ph>
2026-09-08 08:12:17 +0800

    Create student, books, and borrow database tables

---
+++ backups/database/migration/001_initial_library.sql
@@ -0,0 +1,68 @@
+-- #1 student table
+CREATE TABLE student (
+    -- Primary key for the student table
+ student_id INT AUTO_INCREMENT PRIMARY KEY,
+
+-- student name
+student_first_name VARCHAR(50) NOT NULL,
+    student_last_name VARCHAR(50) NOT NULL,
+
+    --student course
+    student_course VARCHAR(50) NOT NULL,
+
+-- student created at timestamp
+    student_created_at TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+
+)ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4
+  COLLATE=utf8mb4_general_ci;
+
+-- #2 book table
+CREATE TABLE IF NOT EXISTS books (
+    -- book deatails
+    book_title VARCHAR(50) NOT NULL,
+    book_author VARCHAR(100) NOT NULL,
+    book_category VARCHAR(50) NOT NULL,
+
+-- Book created at timestamp
+    book_created_at TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+
+)ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4
+  COLLATE=utf8mb4_general_ci;
+
+  --# 3 borrow table
+  CREATE TABLE IF NOT EXISTS borrow (
+    -- Primary key for the borrow table
+    borrow_id INT AUTO_INCREMENT PRIMARY KEY,
+
+    --forreign key references
+    student_id INT NOT NULL,
+    book_id INT NOT NULL,
+
+    -- Borrow timestamp not null by default
+    borrow_date TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+
+    -- Borrow return timestamp null by defaul
+    borrow_return_date TIMESTAMP NULL
+        DEFAULT NULL,
+   
+   --Borrow table constraints and foreign key
+   CONSTRAINT fk_borrow_student
+        FOREIGN KEY (student_id)
+        REFERENCES students(student_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+
+CONSTRAINT fk_borrow_book
+        FOREIGN KEY (book_id)
+        REFERENCES books(book_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+
+  )ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4
+  COLLATE=utf8mb4_general_ci;
