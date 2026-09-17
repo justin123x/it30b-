64c103d882b0914cc9cf3e3c88a68bcd16d0a9bd
justin123x <20240217@nbsc.edu.ph>
2026-09-17 08:53:56 +0800

    Added student listing and library navigation

--- index.php
+++ index.php
@@ -21,6 +21,27 @@ try {
     die("Database connection failed" . $e->getMessage());
 }
 
+// Session
+session_start();
+
+// Determine current section
+$section = $_GET['section'] ?? 'students';
+
+// Determine CRUD Operation
+$action = $_GET['action'] ?? '';
+
+// Fetch Students
+if ($section=== 'students'){
+
+$stmt = $pdo->prepare("
+        SELECT *
+        FROM student
+        ORDER BY student_id DESC
+    ");
+    $stmt->execute();
+    $students = $stmt->fetchAll();
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +52,58 @@ try {
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Simply Library System</h1>
+    <nav>
+        <a href="index.php?section=students">Students</a>
+        <a href="index.php?section=books">Books</a>
+        <a href="index.php?section=borrow">Borrow</a>
+</nav>
+<hr>
+<?php if($section === 'students'): ?>
+        <h1>Students</h1>
+       <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Course</th>
+                <th>Created</th>
+                <th>Action</th>
+</tr>
+        </thead>
+        <tbody>
+    <?php foreach($students as $student): ?>
+        <tr>
+            <td>
+            <?=htmlspecialchars($student['student_id']) ?></td>
+    <td>
+            <?=htmlspecialchars($student['student_first_name']) ?></td>
+    <td>
+            <?=htmlspecialchars($student['student_last_name']) ?></td>
+    <td>
+            <?=htmlspecialchars($student['student_course']) ?></td>
+    <td>
+            <?=htmlspecialchars($student['student_created_at']) ?></td>
+    <td>
+        <a href="index.php?action=edit&id=<?=htmlspecialchars($student['student_id']) ?>">Edit</a>
+
+        <a href="index.php?action=delete&id=<?=htmlspecialchars($student['student_id']) ?>">Delete</a>
+    </td>
+    </tr>
+    <?php endforeach?>
+    </tbody>
+
+    </table>
+
+    <?php endif;?>
+
+        <?php if($section === 'books'): ?>
+        <h1>Books</h1>
+        <?php endif;?>
+
+        <?php if($section === 'borrow'): ?>
+        <h1>Borrow</h1>
+        <?php endif;?>
 </body>
 </html>
